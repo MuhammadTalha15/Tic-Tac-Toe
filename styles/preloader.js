d2a7de2c7ed31a8abc1a9529800c8ba8a46a3d8a
Muhammad Talha <141347189+MuhammadTalha15@users.noreply.github.com>
2024-02-07 20:50:14 +0500

    Update preloader.js

--- styles/preloader.js
+++ styles/preloader.js
@@ -1,15 +1,16 @@
 
 const loadView = document.getElementById('preloader');
 const progress = document.getElementById('progress');
+let loadbar_width = document.getElementById('loadbar');
 
-window.addEventListener('load', async () => {
+window.addEventListener('load', () => {
     progress.style.width = '99%';
     let width_of_Prog = '';
+    let actual_width = '';
     setTimeout(() => {
-        width_of_Prog += window.getComputedStyle(progress).getPropertyValue('width');
-        console.log(width_of_Prog);
-        if (width_of_Prog === '396px') {
-            console.log('done')
+        width_of_Prog += parseInt(window.getComputedStyle(progress).getPropertyValue('width'));
+        actual_width += parseInt(window.getComputedStyle(loadbar_width).getPropertyValue('width'));
+        if (width_of_Prog < actual_width) {
             loadView.style.opacity = '0';
             setTimeout(()=>{
                 loadView.style.display = 'none';
